15e264c40dca47524cecb1530a450a678bea27e5
Ozan1907-design <83665028+Ozan1907-design@users.noreply.github.com>
2025-11-11 13:29:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "5be145b3-504f-4936-90e8-9260c2e5806f"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstateozan"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
@@ -25,3 +25,4 @@ resource "azurerm_resource_group" "example" {
   name     = "RG2"
   location = "westeurope"
 }
+
